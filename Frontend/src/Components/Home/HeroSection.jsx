c832de192d81089dce7363ac157386bf1fd4e50f
Dipa Joshi <joshideepa988@gmail.com>
2025-06-14 00:25:26 +0545

    name renamed

--- Frontend/src/Components/Home/HeroSection.jsx
+++ Frontend/src/Components/Home/HeroSection.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from 'react-router-dom';
-import image from '../../assets/Homeimage.jpg';
+import image from '../../assets/homepic.jpg';
 
 const HeroSection = () => {
   return (
