b8ad16812492ac81b4c9cab7dc1b3f45029a7b8b
Dipa Joshi <joshideepa988@gmail.com>
2025-06-14 09:42:50 +0545

    khet input component added

--- Frontend/src/Pages/KhetTracker.jsx
+++ Frontend/src/Pages/KhetTracker.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from '../Components/Header'
+import KhetInputForm from '../Components/Khet Tracker/KhetInput'
 
 function KhetTracker() {
   return (
     <div>
         <Header/>
+        <KhetInputForm/>
       KHet tracekr
     </div>
   )
