b831fdf8b1c83165df3e6316133322aa256679ee
Dipa Joshi <joshideepa988@gmail.com>
2025-06-15 00:46:46 +0545

    design refined

--- Frontend/src/Pages/KhetTracker.jsx
+++ Frontend/src/Pages/KhetTracker.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../Components/Header'
 import CreateKhet from '../Components/Khet Tracker/CreateKhet'
+import Footer from '../Components/Footer'
 
 function KhetTracker() {
   return (
     <div className='bg-green-50'>
         <Header/>
         <CreateKhet/>
+        <Footer/>
     </div>
   )
 }
